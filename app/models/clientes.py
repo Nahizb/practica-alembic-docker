1e0e15f9db529769ab0ab53f963b2574c5d1291e
Nahizbeth <nahizbeth.zerpa95@gmail.com>
2026-07-14 20:11:41 +0200

    Agregar columna telefono a clientes

--- app/models/clientes.py
+++ app/models/clientes.py
@@ -9,3 +9,4 @@ class Cliente(Base):
     id_cliente = Column(Integer, primary_key=True)
     nombre_cliente = Column(String(45), nullable=False)
     dni_cliente = Column(String(12), nullable=False, unique=True)
+    telefono = Column(String(20))  # ← Nuevo campo prueba githubactions
